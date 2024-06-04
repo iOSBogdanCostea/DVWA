ed22eb39bfcab9979586782068e80d97e9d007ad
iOSBogdanCostea <71764912+iOSBogdanCostea@users.noreply.github.com>
2024-06-04 23:05:04 +0300

    Update high.php

--- vulnerabilities/csrf/source/high.php
+++ vulnerabilities/csrf/source/high.php
@@ -1,69 +1,47 @@
 <?php
 
-$change = false;
-$request_type = "html";
-$return_message = "Request Failed";
-
-if ($_SERVER['REQUEST_METHOD'] == "POST" && array_key_exists ("CONTENT_TYPE", $_SERVER) && $_SERVER['CONTENT_TYPE'] == "application/json") {
-	$data = json_decode(file_get_contents('php://input'), true);
-	$request_type = "json";
-	if (array_key_exists("HTTP_USER_TOKEN", $_SERVER) &&
-		array_key_exists("password_new", $data) &&
-		array_key_exists("password_conf", $data) &&
-		array_key_exists("Change", $data)) {
-		$token = $_SERVER['HTTP_USER_TOKEN'];
-		$pass_new = $data["password_new"];
-		$pass_conf = $data["password_conf"];
-		$change = true;
-	}
-} else {
-	if (array_key_exists("user_token", $_REQUEST) &&
-		array_key_exists("password_new", $_REQUEST) &&
-		array_key_exists("password_conf", $_REQUEST) &&
-		array_key_exists("Change", $_REQUEST)) {
-		$token = $_REQUEST["user_token"];
-		$pass_new = $_REQUEST["password_new"];
-		$pass_conf = $_REQUEST["password_conf"];
-		$change = true;
-	}
-}
-
-if ($change) {
+if( isset( $_GET[ 'Change' ] ) ) {
 	// Check Anti-CSRF token
-	checkToken( $token, $_SESSION[ 'session_token' ], 'index.php' );
+	checkToken( $_REQUEST[ 'user_token' ], $_SESSION[ 'session_token' ], 'index.php' );
 
-	// Do the passwords match?
-	if( $pass_new == $pass_conf ) {
-		// They do!
-		$pass_new = mysqli_real_escape_string ($GLOBALS["___mysqli_ston"], $pass_new);
+	// Get input
+	$pass_curr = $_GET[ 'password_current' ];
+	$pass_new  = $_GET[ 'password_new' ];
+	$pass_conf = $_GET[ 'password_conf' ];
+
+	// Sanitise current password input
+	$pass_curr = stripslashes( $pass_curr );
+	$pass_curr = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $pass_curr ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
+	$pass_curr = md5( $pass_curr );
+
+	// Check that the current password is correct
+	$data = $db->prepare( 'SELECT password FROM users WHERE user = (:user) AND password = (:password) LIMIT 1;' );
+	$current_user = dvwaCurrentUser();
+	$data->bindParam( ':user', $current_user, PDO::PARAM_STR );
+	$data->bindParam( ':password', $pass_curr, PDO::PARAM_STR );
+	$data->execute();
+
+	// Do both new passwords match and does the current password match the user?
+	if( ( $pass_new == $pass_conf ) && ( $data->rowCount() == 1 ) ) {
+		// It does!
+		$pass_new = stripslashes( $pass_new );
+		$pass_new = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $pass_new ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
 		$pass_new = md5( $pass_new );
 
-		// Update the database
+		// Update database with new password
+		$data = $db->prepare( 'UPDATE users SET password = (:password) WHERE user = (:user);' );
+		$data->bindParam( ':password', $pass_new, PDO::PARAM_STR );
 		$current_user = dvwaCurrentUser();
-		$insert = "UPDATE `users` SET password = '" . $pass_new . "' WHERE user = '" . $current_user . "';";
-		$result = mysqli_query($GLOBALS["___mysqli_ston"],  $insert );
+		$data->bindParam( ':user', $current_user, PDO::PARAM_STR );
+		$data->execute();
 
 		// Feedback for the user
-		$return_message = "Password Changed.";
+		$html .= "<pre>Password Changed.</pre>";
 	}
 	else {
 		// Issue with passwords matching
-		$return_message = "Passwords did not match.";
-	}
-
-	mysqli_close($GLOBALS["___mysqli_ston"]);
-
-	if ($request_type == "json") {
-		generateSessionToken();
-		header ("Content-Type: application/json");
-		print json_encode (array("Message" =>$return_message));
-		exit;
-	} else {
-		$html .= "<pre>" . $return_message . "</pre>";
+		$html .= "<pre>Passwords did not match or current password incorrect.</pre>";
 	}
 }
-
-// Generate Anti-CSRF token
-generateSessionToken();
 
 ?>
