31ce6abc4bbcb19adbc83c8dd8936c8eb04517b5
iOSBogdanCostea <71764912+iOSBogdanCostea@users.noreply.github.com>
2024-06-04 22:27:16 +0300

    Update medium.php

--- vulnerabilities/xss_d/source/medium.php
+++ vulnerabilities/xss_d/source/medium.php
@@ -2,12 +2,18 @@
 
 // Is there any input?
 if ( array_key_exists( "default", $_GET ) && !is_null ($_GET[ 'default' ]) ) {
-	$default = $_GET['default'];
-	
-	# Do not allow script tags
-	if (stripos ($default, "<script") !== false) {
-		header ("location: ?default=English");
-		exit;
+
+	# White list the allowable languages
+	switch ($_GET['default']) {
+		case "French":
+		case "English":
+		case "German":
+		case "Spanish":
+			# ok
+			break;
+		default:
+			header ("location: ?default=English");
+			exit;
 	}
 }
 
