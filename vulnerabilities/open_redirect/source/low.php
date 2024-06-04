f2443c97b9174284c6fc2d9feb9ffb9c57e3435b
iOSBogdanCostea <71764912+iOSBogdanCostea@users.noreply.github.com>
2024-06-04 22:48:49 +0300

    Update low.php

--- vulnerabilities/open_redirect/source/low.php
+++ vulnerabilities/open_redirect/source/low.php
@@ -1,13 +1,29 @@
 <?php
 
-if (array_key_exists ("redirect", $_GET) && $_GET['redirect'] != "") {
-	header ("location: " . $_GET['redirect']);
-	exit;
+$target = "";
+
+if (array_key_exists ("redirect", $_GET) && is_numeric($_GET['redirect'])) {
+	switch (intval ($_GET['redirect'])) {
+		case 1:
+			$target = "info.php?id=1";
+			break;
+		case 2:
+			$target = "info.php?id=2";
+			break;
+		case 99:
+			$target = "https://digi.ninja";
+			break;
+	}
+	if ($target != "") {
+		header ("location: " . $target);
+		exit;
+	} else {
+		?>
+		Unknown redirect target.
+		<?php
+		exit;
+	}
 }
 
-http_response_code (500);
 ?>
-<p>Missing redirect target.</p>
-<?php
-exit;
-?>
+Missing redirect target.
